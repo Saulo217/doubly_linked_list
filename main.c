151d1369c070c8af2c1f72e4771ea2809ad51183
saulo217 <78312710+Saulo217@users.noreply.github.com>
2025-07-11 19:57:20 -0300

    update: change main file to include the new library

--- main.c
+++ main.c
@@ -1,31 +1,33 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include "lib/doubly_linked_list.h"
+
+void print_data(int data)
+{
+  printf("%d\n", data);
+}
 
 int main(int argc, char *argv[])
 {
-  typedef struct Node {
-    int data;
-    node* prev;
-    node* next;
-  } node;
-
-  node* root;
   node first, second, third;
 
-  root = &first;
-
-  first->data = 10;
-  first->prev = NULL;
-  first->next = &second;
+  first.data = 10;
+  first.prev = NULL;
+  first.next = &second;
   
-  second->data = 20;
-  second->prev = &first;
-  second->next = &third;
+  second.data = 20;
+  second.prev = &first;
+  second.next = &third;
 
-  third->data = 20;
-  third->prev = &second;
-  third->next = NULL;
+  third.data = 30;
+  third.prev = &second;
+  third.next = NULL;
 
+  printf("%s\n", "Forward Traverse:");
+  forward_traverse(&first, print_data);
+
+  printf("%s\n", "Backward Traverse:");
+  backward_traverse(&third, print_data);
   return EXIT_SUCCESS;
 }
